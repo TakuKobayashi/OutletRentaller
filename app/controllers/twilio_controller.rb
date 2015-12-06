73899ad223158de2ab3b1bfa9d1979cfcb966ba2
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2015-12-06 16:43:38 +0900

    電話の文言を追加

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -2,6 +2,14 @@ class TwilioController < BaseController
   skip_before_filter :verify_authenticity_token
 
   def request_xml
-    render xml: Gyoku.xml({Response: {Say: {content!: "あさいさんかっこいいです。わーーーーーーー",  "@language" => "ja-jp"}}}, {key_converter: :camelcase}) and return
+    render xml: Gyoku.xml({
+      Response: {
+        Say:{
+          content!: "お貸ししております。電源タップの返却時間を過ぎておりますが、ご返却いただいておりません。ご返却をお願いします。", 
+          "@language" => "ja-jp"
+          }
+       }
+     },{key_converter: :camelcase}
+     ) and return
   end
 end
