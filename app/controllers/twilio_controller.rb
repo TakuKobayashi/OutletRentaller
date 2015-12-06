5b0624b84d89aa4fb49dcf65195e42703ff0b615
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2015-12-06 13:57:46 +0900

    CSRFを外さないとダメだった

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,4 +1,6 @@
 class TwilioController < BaseController
+  skip_before_filter :verify_authenticity_token
+
   def request_xml
     render xml: Gyoku.xml({Response: {Say: {content!: "あさいさんかっこいいです。わーーーーーーー",  "@language" => "ja-jp"}}}, {key_converter: :camelcase}) and return
   end
